d6f2b50d279ed11fa0146e210477463832348273
bramvgit <bramverdouw@outlook.com>
2021-04-01 20:46:24 +0200

    Refactored

--- startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
+++ startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
@@ -105,36 +105,40 @@ public class ASTListener extends ICSSBaseListener {
                 } else {
                     addLiteral(value);
                 }
-                mergeDeclaration();
             } else {
-                ParseTree startValue = value.getChild(0);
-                ParseTree operator = value.getChild(1);
-                ParseTree calculation = value.getChild(2);
-
-                Operation operation = getOperation(operator.getText());
-
-                if (isVariableReference(startValue)) {
-                    addVariableReferenceToOperation(startValue, operation);
-                } else {
-                    throw new NullPointerException("Not Implemented");
-                }
-
-                String val1 = calculation.getChild(0).getChild(0).getText();
-                String op = calculation.getChild(0).getChild(1).getText();
-                String val2 = calculation.getChild(0).getChild(2).getText();
-
-                Literal l1 = getLiteralWithScalar(val1);
-                Literal l2 = getLiteralWithScalar(val2);
-                Operation op1 = getOperation(op);
-
-                op1.addChild(l1);
-                op1.addChild(l2);
-
-                operation.addChild(op1);
-
-                mergeDeclaration();
+                addCalculation(value);
             }
+            mergeDeclaration();
         }
+    }
+
+    private void addCalculation(ParseTree value) {
+        ParseTree startValue = value.getChild(0);
+        ParseTree operator = value.getChild(1);
+        ParseTree calculation = value.getChild(2);
+
+        // Add operation
+        Operation operation = getOperation(operator.getText());
+
+        if (isVariableReference(startValue)) {
+            addVariableReferenceToOperation(startValue, operation);
+        } else {
+            throw new NullPointerException("Not Implemented");
+        }
+
+        // Add calculation values
+        String val1 = calculation.getChild(0).getChild(0).getText();
+        String op = calculation.getChild(0).getChild(1).getText();
+        String val2 = calculation.getChild(0).getChild(2).getText();
+
+        Literal l1 = getLiteralWithScalar(val1);
+        Literal l2 = getLiteralWithScalar(val2);
+        Operation op1 = getOperation(op);
+
+        op1.addChild(l1);
+        op1.addChild(l2);
+
+        operation.addChild(op1);
     }
 
     private void addVariableReferenceToOperation(ParseTree startValue, Operation operation) {
