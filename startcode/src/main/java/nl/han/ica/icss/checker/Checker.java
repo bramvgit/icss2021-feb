4578a6ace7307b2fba5ca0d2c892d1e867f1a455
bramvgit <bramverdouw@outlook.com>
2021-04-04 19:04:04 +0200

    Added check CH01 | undefined variable reference

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -3,6 +3,9 @@ package nl.han.ica.icss.checker;
 import nl.han.ica.datastructures.HANLinkedList;
 import nl.han.ica.datastructures.IHANLinkedList;
 import nl.han.ica.icss.ast.AST;
+import nl.han.ica.icss.ast.Declaration;
+import nl.han.ica.icss.ast.Stylerule;
+import nl.han.ica.icss.ast.VariableAssignment;
 import nl.han.ica.icss.ast.types.ExpressionType;
 
 import java.util.HashMap;
@@ -14,5 +17,25 @@ public class Checker {
 
     public void check(AST ast) {
         variableTypes = new HANLinkedList<>();
+        ast.root.body.forEach(part -> {
+            // Variable assignment
+            if (part instanceof VariableAssignment) {
+
+            }
+
+            // Style rule
+            if (part instanceof Stylerule) {
+                Stylerule stylerule = (Stylerule) part;
+                stylerule.body.forEach(body -> {
+                    if (body instanceof Declaration) {
+                        Declaration declaration = (Declaration) body;
+
+                        if (declaration.expression == null) {
+                            declaration.setError(declaration.property + " uses undefined variable reference");
+                        }
+                    }
+                });
+            }
+        });
     }
 }
