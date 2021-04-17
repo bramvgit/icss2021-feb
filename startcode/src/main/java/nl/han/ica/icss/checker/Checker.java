b59dd68829eebd7bf97c190bb827fc91aff27ce8
bramvgit <bramverdouw@outlook.com>
2021-04-17 23:40:30 +0200

    Added property value checking

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -1,26 +1,29 @@
 package nl.han.ica.icss.checker;
 
-import nl.han.ica.datastructures.HANLinkedList;
-import nl.han.ica.datastructures.IHANLinkedList;
-import nl.han.ica.icss.ast.AST;
-import nl.han.ica.icss.ast.Declaration;
-import nl.han.ica.icss.ast.Stylerule;
-import nl.han.ica.icss.ast.VariableAssignment;
-import nl.han.ica.icss.ast.types.ExpressionType;
+import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.literals.PercentageLiteral;
+import nl.han.ica.icss.ast.literals.PixelLiteral;
 
 import java.util.HashMap;
+import java.util.Map;
 
 
 public class Checker {
 
-    private IHANLinkedList<HashMap<String, ExpressionType>> variableTypes;
+    //private IHANLinkedList<HashMap<String, ExpressionType>> variableTypes;
+    private Map<String, Expression> variableTypes;
+
+    private Map<String, String> propertyTypes;
 
     public void check(AST ast) {
-        variableTypes = new HANLinkedList<>();
+        variableTypes = new HashMap<>();
+        propertyTypes = new HashMap<>();
+
         ast.root.body.forEach(part -> {
             // Variable assignment
             if (part instanceof VariableAssignment) {
-
+                VariableAssignment variableAssignment = (VariableAssignment) part;
+                variableTypes.put(variableAssignment.name.name, variableAssignment.expression);
             }
 
             // Style rule
@@ -30,12 +33,34 @@ public class Checker {
                     if (body instanceof Declaration) {
                         Declaration declaration = (Declaration) body;
 
+                        // Undefined variable
                         if (declaration.expression == null) {
                             declaration.setError(declaration.property + " uses undefined variable reference");
+                        }
+
+                        // Invalid value for property (for variables)
+                        if (declaration.expression instanceof VariableReference) {
+                            Expression value = variableTypes.get(((VariableReference) declaration.expression).name);
+
+                            if (declaration.property.name.equalsIgnoreCase("width") && !(value instanceof PixelLiteral)) {
+                                checkPropertyValue(declaration, value);
+                            }
+                        }
+                        // Invalid value for property (only hardcoded)
+                        else if (declaration.property.name.equalsIgnoreCase("width") && !(declaration.expression instanceof PixelLiteral)) {
+                            checkPropertyValue(declaration, declaration.expression);
                         }
                     }
                 });
             }
         });
+    }
+
+    private void checkPropertyValue(Declaration declaration, Expression value) {
+        if (declaration.property.name.equalsIgnoreCase("width")) {
+            if (!(value instanceof PixelLiteral) && !(value instanceof PercentageLiteral)) {
+                declaration.setError(declaration.property.name + " only accepts pixels or percentage values!");
+            }
+        }
     }
 }
