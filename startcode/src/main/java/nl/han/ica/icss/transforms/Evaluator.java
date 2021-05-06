5dca96b4d849e9c33190fd1e09fe3dc724593346
bramvgit <bramverdouw@outlook.com>
2021-05-06 16:57:20 +0200

    Added initial transformer

--- startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
+++ startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
@@ -1,30 +1,129 @@
 package nl.han.ica.icss.transforms;
 
-import nl.han.ica.datastructures.IHANLinkedList;
 import nl.han.ica.icss.ast.*;
-import nl.han.ica.icss.ast.literals.PercentageLiteral;
-import nl.han.ica.icss.ast.literals.PixelLiteral;
-import nl.han.ica.icss.ast.literals.ScalarLiteral;
-import nl.han.ica.icss.ast.operations.AddOperation;
-import nl.han.ica.icss.ast.operations.MultiplyOperation;
-import nl.han.ica.icss.ast.operations.SubtractOperation;
+import nl.han.ica.icss.ast.literals.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class Evaluator implements Transform {
 
-    private IHANLinkedList<HashMap<String, Literal>> variableValues;
+    private final Map<String, Literal> variables;
+    private final List<ASTNode> removedNodes;
 
     public Evaluator() {
-        //variableValues = new HANLinkedList<>();
+        variables = new HashMap<>();
+        removedNodes = new ArrayList<>();
     }
 
     @Override
     public void apply(AST ast) {
-        //variableValues = new HANLinkedList<>();
-
+        traverse(ast.root);
     }
 
-    
+    private void traverse(List<ASTNode> nodes) {
+        for (ASTNode node : nodes) {
+            traverse(node);
+        }
+    }
+
+    private void traverse(ASTNode node) {
+        if (node instanceof Stylesheet) {
+            transformStyleSheet((Stylesheet) node);
+        } else if (node instanceof Stylerule) {
+            transformStylerule((Stylerule) node);
+        } else if (node instanceof VariableAssignment) {
+            transformVariableAssignment((VariableAssignment) node);
+        } else if (node instanceof IfClause) {
+            transformIfClause((IfClause) node);
+        } else if (node instanceof ElseClause) {
+            transformElseClause((ElseClause) node);
+        } else if (node instanceof Operation) {
+            transformOperation((Operation) node);
+        } else if (node instanceof Declaration) {
+            transformDeclaration((Declaration) node);
+        }
+    }
+
+    private void transformDeclaration(Declaration declaration) {
+        for (ASTNode node : declaration.getChildren()) {
+            if (node instanceof VariableReference) {
+                // TODO: replace with literal
+            } else {
+                traverse(node);
+            }
+        }
+    }
+
+    private void transformElseClause(ElseClause elseClause) {
+        traverse(elseClause.getChildren());
+        removedNodes.add(elseClause);
+    }
+
+    private void transformIfClause(IfClause ifClause) {
+        for (ASTNode node : ifClause.getChildren()) {
+            if (node instanceof VariableReference) {
+                // TODO: replace with literal
+            } else {
+                traverse(node);
+            }
+        }
+        removedNodes.add(ifClause);
+    }
+
+    private void transformStyleSheet(Stylesheet stylesheet) {
+        traverse(stylesheet.getChildren());
+        for (ASTNode node : removedNodes) {
+            stylesheet.removeChild(node);
+        }
+    }
+
+    private void transformStylerule(Stylerule stylerule) {
+//        ArrayList<ASTNode> nodesToKeep = new ArrayList<>();
+        traverse(stylerule.getChildren());
+//        for (ASTNode node : stylerule.getChildren()) {
+//            if (!removedNodes.contains(node)) nodesToKeep.add(node);
+//        }
+//        stylerule.body = nodesToKeep;
+    }
+
+    private void transformVariableAssignment(VariableAssignment variableAssignment) {
+        for (ASTNode node : variableAssignment.getChildren()) {
+            if (!(node instanceof VariableReference)) {
+                traverse(node);
+            }
+        }
+
+        // TODO: literal throws error when in operation
+        variables.put(variableAssignment.name.name, getLiteral(variableAssignment.expression));
+        removedNodes.add(variableAssignment);
+    }
+
+    private void transformOperation(Operation operation) {
+        for (ASTNode node : operation.getChildren()) {
+            if (node instanceof VariableReference) {
+                // TODO: replace with literal
+            } else {
+                traverse(node);
+            }
+        }
+    }
+
+    private Literal getLiteral(Expression expression) {
+        Literal literal;
+        if (expression instanceof PixelLiteral) {
+            literal = (PixelLiteral) expression;
+        } else if (expression instanceof PercentageLiteral) {
+            literal = (PercentageLiteral) expression;
+        } else if (expression instanceof ScalarLiteral) {
+            literal = (ScalarLiteral) expression;
+        } else if (expression instanceof BoolLiteral) {
+            literal = (BoolLiteral) expression;
+        } else {
+            literal = (ColorLiteral) expression;
+        }
+        return literal;
+    }
 }
